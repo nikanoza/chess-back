a090821526242f13d51049d575e0dc53f0ea1f40
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-04 13:01:45 +0400

    repactor: change login request function

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,21 +1,41 @@
 import { Request, Response } from "express";
-import { User } from "models";
+import { sendEmailVerification } from "mail/edge";
+import { EmailVerify, User } from "models";
 import { registerSchema } from "schemas";
 import { v4 as uuid } from "uuid";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 export const register = async (req: Request, res: Response) => {
   try {
     const { username, email, password, repeatPassword } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const hash = crypto.randomBytes(48).toString("hex");
     const value = await registerSchema.validateAsync({
       username,
       email,
       password,
       repeatPassword,
     });
+
     const id = uuid();
-    value.id = id;
-    const user = new User(value);
+
+    const user = new User({
+      username: value.username,
+      email: value.email,
+      password: hashedPassword,
+      purchase: [],
+      id,
+    });
     await user.save();
+    await EmailVerify.create({
+      hash,
+      email,
+    });
+
+    await sendEmailVerification(email, hash, username, backLink);
     return res.send(user);
   } catch (error) {
     return res.status(400).send(error);
