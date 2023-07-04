9ed7fb6c29e43bcadc6990d15065cc3d02186925
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-04 10:52:24 +0400

    feat: add nodemailer to project

---
+++ src/mail/edge.ts
@@ -0,0 +1,46 @@
+import { Edge } from "edge.js";
+import path, { join } from "path";
+import mailTransport from "./index";
+
+const edge = new Edge({ cache: false });
+const templatesPath = join(path.resolve(), "src/mail/templates");
+edge.mount(templatesPath);
+
+const send = (to: string, subject: string, html: any) => {
+  const options = {
+    to,
+    subject,
+    html,
+    from: "",
+  };
+
+  return mailTransport.sendMail(options);
+};
+
+// export const sendPasswordRecovery = async (
+//   to: string,
+//   hash: string,
+//   name: string,
+//   backLink: string
+// ) => {
+//   const html = edge.renderSync("password-recovery", {
+//     link: `${backLink}?hash=${hash}`,
+//     name,
+//   });
+
+//   return send(to, "Password Reset", html);
+// };
+
+export const sendEmailVerification = async (
+  to: string,
+  hash: string,
+  name: string,
+  backLink: string
+) => {
+  const html = edge.renderSync("user-verify", {
+    link: `${backLink}?hash=${hash}`,
+    name,
+  });
+
+  return send(to, "Verify User", html);
+};
