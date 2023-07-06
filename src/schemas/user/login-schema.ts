63cf82688c843caf7830728b9ebf69e4b31cde8f
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-06 15:08:24 +0400

    feat: add login validation schema

--- src/schemas/user/login-schema.ts
+++ src/schemas/user/login-schema.ts
@@ -2,26 +2,21 @@ import Joi from "joi";
 import { User } from "models";
 
 const loginSchema = Joi.object({
-  username: Joi.string()
-    .required()
-    .external(async (value) => {
-      const user = await User.findOne({ username: value });
-      if (user) {
-        throw new Error("Username already exists");
-      }
-    }),
   email: Joi.string()
     .email()
     .required()
-    .external(async (value) => {
-      const user = await User.findOne({ email: value });
-      if (user) {
-        throw new Error("Email already exists");
+    .custom(async (value, helpers) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (!user) {
+          throw new Error("Invalid email or password");
+        }
+        return value;
+      } catch (err) {
+        return helpers.error("any.invalid");
       }
     }),
-  password: Joi.string().min(8).max(15).required(),
-  repeatPassword: Joi.ref("password"),
-  redirectLink: Joi.string().uri().required(),
+  password: Joi.string().required(),
 });
 
 export default loginSchema;
