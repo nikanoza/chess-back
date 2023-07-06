bbf9982756f7eeffeb9a762f69be773d7674f0b3
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-06 15:43:57 +0400

    feat: create login route & add to swagger

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,9 +1,10 @@
-import { getAllUser, register } from "controllers";
+import { getAllUser, login, register } from "controllers";
 import express from "express";
 
 const userRouter = express.Router();
 
 userRouter.get("/users", getAllUser);
 userRouter.post("/register", register);
+userRouter.post("/login", login);
 
 export default userRouter;
