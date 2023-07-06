07711bf7218be11e726ad9d1ab0ec6358b9cdd82
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-06 15:25:41 +0400

    feat: create login request function

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { sendEmailVerification } from "mail/edge";
 import { EmailVerify, User } from "models";
-import { registerSchema } from "schemas";
+import { loginSchema, registerSchema } from "schemas";
 import { v4 as uuid } from "uuid";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -44,4 +45,39 @@ export const register = async (req: Request, res: Response) => {
   }
 };
 
-export const login = async (req: Request, res: Response) => {};
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const value = await loginSchema.validateAsync({
+      email,
+      password,
+    });
+
+    const user = await User.findOne({ email: value.email }).select("+password");
+
+    if (!user) {
+      return res.status(400).json({ message: "user not found!" });
+    }
+    const result = await bcrypt.compare(password, user.password);
+
+    if (result) {
+      const signData = {
+        username: user.username,
+        email: user.email,
+        address: user.address,
+        purchase: user.purchase,
+        userId: user.id,
+      };
+
+      const token = jwt.sign(signData, process.env.JWT_SECRET!);
+      return res.json({ ...signData, token });
+    }
+    return res
+      .status(401)
+      .json({ message: "please, provide correct credentials..." });
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "please, provide correct credentials..." });
+  }
+};
--- src/schemas/user/index.ts
+++ src/schemas/user/index.ts
@@ -1,1 +1,2 @@
 export { default as registerSchema } from "./register-schema.js";
+export { default as loginSchema } from "./login-schema.js";
