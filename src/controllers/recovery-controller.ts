558760ad87cfdc288c59f2e88a74ce680b8b42c9
nixena <nikanozadze1010@gmail.com>
2023-07-17 11:04:36 +0400

    feat: create password reset request function

--- src/controllers/recovery-controller.ts
+++ src/controllers/recovery-controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import crypto from "crypto";
 import { PasswordRecovery, User } from "models";
 import { sendRecoveryMail } from "mail/edge";
+import { passwordRecoverySchema } from "schemas";
+import bcrypt from "bcrypt"
 
 export const sendRecoveryRequest = async (req: Request, res: Response) => {
   const { email, backLink } = req.body;
@@ -29,3 +31,34 @@ export const sendRecoveryRequest = async (req: Request, res: Response) => {
     .status(401)
     .json({ message: "please, provide correct credentials..." });
 };
+
+export const passwordReset = async (req: Request, res:Response) => {
+  try {
+    const { password, repeatPassword, hash } =
+      req.body;
+    await passwordRecoverySchema.validateAsync({
+      password,
+      repeatPassword,
+      hash
+    })
+    const verifyDocument = await PasswordRecovery.findOne({hash});
+
+    if(!verifyDocument){
+      return res
+      .status(401)
+      .json({ message: "can't update password, something went wrong" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await User.findOneAndUpdate({ email: verifyDocument.email}, { password: hashedPassword});
+
+    return res.status(200).json({message: "password update successfully"})
+
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "can't update password, something went wrong" });
+  }
+}
