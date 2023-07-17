bfa6126921f8cd15128e0d386ef35075cd5d9ae7
nixena <nikanozadze1010@gmail.com>
2023-07-17 12:39:28 +0400

    feat: create route for verification

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,10 +1,11 @@
-import { getAllUser, login, register } from "controllers";
+import { getAllUser, login, register, verifyAccount } from "controllers";
 import express from "express";
 
 const userRouter = express.Router();
 
 userRouter.get("/users", getAllUser);
 userRouter.post("/register", register);
+userRouter.post("/verify", verifyAccount);
 userRouter.post("/login", login);
 
 export default userRouter;
